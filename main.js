b45b0e2e9afadaaabc2d8c7d5f65ebfd3aec0099
unknown <thekoolega123@gmail.com>
2020-02-28 04:26:09 +0100

    text append, UI

--- main.js
+++ main.js
@@ -27,22 +27,6 @@
       }
     });
 
-    const btnToggle = document.querySelector(".m-toggle");
-    const menu = document.querySelector(".m");
-    let toggled = true;
-
-    btnToggle.addEventListener("click", function() {
-      if (toggled) {
-        menu.classList.add("menu-hide");
-        btnToggle.firstChild.classList.remove("lefty");
-        toggled = false;
-      } else {
-        menu.classList.remove("menu-hide");
-        btnToggle.firstChild.classList.add("lefty");
-        toggled = true;
-      }
-    });
-
     const headerToggle = document.querySelector(".fa-sort-down");
     const header = document.querySelector(".header");
     const nav = document.querySelector(".project-nav");
@@ -63,14 +47,16 @@
 (function() {
   const workArea = document.querySelector(".work-area");
   const divCreate = document.querySelector(".div-options");
-  const createText = document.querySelector(".create-new-text");
+  let createText = document.querySelectorAll(".text-create");
   const apiImagesWrapper = document.querySelector(".api-images-wrapper");
 
   let click = 1;
   let currentZIndex = 1;
 
   divCreate.addEventListener("mouseup", createNewObject);
-  createText.addEventListener("click", createNewObject);
+  createText.forEach(function(txt) {
+    txt.addEventListener("click", createNewObject);
+  });
   apiImagesWrapper.addEventListener("click", createNewObject);
 
   function createNewObject(e) {
@@ -79,18 +65,21 @@
 
     if (create.className !== "create-indicator") {
       //div append
+
       var divParent = document.createElement("div");
       divParent.className = "divParent" + click;
+      let div;
 
-      var div = document.createElement("div");
+      if (create.className == "text-create") {
+        div = document.createElement(e.target.tagName);
+      } else {
+        div = document.createElement("div");
+      }
+
       divParent.appendChild(div);
 
       div.className = "div" + click;
       workArea.appendChild(divParent);
-
-      //get style of selected shape
-      //lines
-      const lineWrapper = document.querySelector(".div-line-wrapper");
 
       if (e.target.parentNode.className == "div-shapes-container") {
         var computedStyle = window.getComputedStyle(create);
@@ -109,6 +98,8 @@
       divParent.style.position = "absolute";
       divParent.style.width = div.style.width;
       divParent.style.height = div.style.height;
+
+      //console.log(divParent.style.width);
 
       div.style.margin = "0 auto";
       divParent.style.top = "150px";
@@ -204,25 +195,43 @@
         res.style.display = "none";
       });
 
-      divParent.addEventListener("mouseenter", function() {
-        let parent = this;
-        parent.style.border = "1px solid #2872c7";
-        for (i = 1; i <= parent.childNodes.length - 1; i++) {
-          parent.children[i].style.display = "";
-        }
+      workArea.addEventListener("click", function(e) {
+        let parent = divParent;
+        let isChild = 0;
+
+        parent.childNodes.forEach(function(child) {
+          if (e.target == child) {
+            isChild++;
+          }
+
+          if (isChild == 1 || e.target == menuIcon) {
+            parent.style.border = "1px solid #2872c7";
+            for (i = 1; i <= parent.childNodes.length - 1; i++) {
+              parent.children[i].style.display = "";
+            }
+          } else if (isChild == 0) {
+            parent.style.border = "none";
+            for (i = 1; i <= parent.childNodes.length - 1; i++) {
+              parent.children[i].style.display = "none";
+            }
+          }
+        });
       });
 
-      divParent.addEventListener("mouseleave", function() {
-        let parent = this;
-        parent.style.border = "none";
-        for (i = 1; i <= parent.childNodes.length - 1; i++) {
-          parent.children[i].style.display = "none";
-        }
+      /*
+      config.addEventListener("click", function(e) {
+        document.querySelector(".object-menu").classList.remove("invisible");
       });
+      */
+
+      if (create.className == "text-create") {
+        textCreate();
+      }
 
       ///////////////////
       //SHAPE CONFIG
 
+      console.log("e");
       config.addEventListener("click", updateShape);
       let styleApply = document.querySelectorAll(".style-apply");
       const shapeConfig = document.querySelector(".object-menu");
@@ -251,6 +260,7 @@
         });
 
       function updateShape() {
+        console.log("s");
         let configBtnClassName = this.classList[0];
 
         shapeConfig.classList.remove("invisible");
@@ -336,69 +346,26 @@
 
       //////////////////
       //text create
-      if (create == createText) {
-        const textInput = document.querySelector(".text-input");
-        div.classList.add("text-style");
-        let currentTextDiv;
-        let innerText = document.createElement("div");
-        div.classList.add("border");
+      function textCreate() {
+        divParent.style.height = "40px";
+        divParent.style.width = "200px";
+        div.innerText = e.target.innerText;
+        div.style.fontFamily = e.target.style.fontFamily;
 
-        div.addEventListener("click", function(e) {
-          currentlyWritting = this;
+        let currentText;
 
-          //show/hide border
-          workArea.addEventListener("click", function(e) {
-            if (e.target != div) {
-              if (e.target != div.children[5]) {
-                currentlyWritting.classList.remove("border");
-              }
-
-              currentlyWritting.classList.remove("border");
-            } else {
-              currentlyWritting.classList.add("border");
-            }
-          });
-
-          //input from innerText to textInput
-          if (currentlyWritting.children[5].innerHTML != "") {
-            textInput.value = currentlyWritting.children[5].innerHTML;
-          }
-
-          //text input
-          textInput.addEventListener("keyup", function() {
-            currentlyWritting.children[5].innerHTML = textInput.value;
-          });
-
-          //font size
-          const fontSizeInput = document.querySelector(".font-size-input");
-          fontSizeInput.addEventListener("change", function() {
-            currentlyWritting.children[5].style.fontSize =
-              fontSizeInput.value + "px";
-          });
-
-          //font family
-          const selectFont = document.querySelector(".select-font");
-          selectFont.addEventListener("change", function() {
-            currentlyWritting.style.fontFamily = selectFont.value;
-          });
-
-          //text background
-          const bgColorInput = document.querySelector(
-            ".background-color-input"
-          );
-          bgColorInput.addEventListener("keyup", function() {
-            currentlyWritting.style.background = bgColorInput.value;
-            let bgSample = document.querySelector(".background-preview");
-
-            bgSample.style.background = bgColorInput.value;
-          });
+        div.addEventListener("dblclick", function() {
+          this.setAttribute("contenteditable", "true");
+          currentText = this;
+          console.log(currentText);
         });
 
-        if (currentTextDiv !== undefined) {
-          currentTextDiv.appendChild(innerText);
-        } else {
-          div.appendChild(innerText);
-        }
+        workArea.addEventListener("click", function(e) {
+          if (e.target !== currentText || e.target !== currentText.parentNode) {
+            div.setAttribute("contenteditable", "false");
+            console.log("s");
+          }
+        });
       }
 
       /////////////
@@ -449,8 +416,6 @@
 
       for (let resizer of resizers) {
         resizer.addEventListener("mousedown", mousedown);
-
-        console.log(el);
 
         function mousedown(e) {
           currentResizer = e.target;
@@ -628,12 +593,22 @@
           document.onmousemove = null;
         }
       }
+      ////////////
+      // LOCAL STORAGE
+      /*
+      saveBtn = document.querySelector(".save-project");
+      saveBtn.addEventListener("click", function() {
+        let myObj = JSON.stringify(divParent);
+        localStorage.setItem("divParent", myObj);
+        console.log(localStorage);
+      });
+      */
     }
   }
 })();
 
 ////////////////////
-//TODO
+//     TODO
 // - może jakies template typy - header, right menu , content
 // - pokazywanie który div jest teraz konfigurowany przez menu z prawej
 // - wincyj opcji!
@@ -646,6 +621,8 @@
 // - zrobic kształty tak samo jak tekst. główny div i do niego append kształtu
 // - ogarnąć placement resizerów oraz menu i del buttonów po powiekszeniu bordera
 
+// !!! // - ogarnąć poruszanie diva myszką, podpiąć divy i menu config pod 1 skrypt
+
 //////////////////
 // BUGI
 // - show/hide border na tekscie nie działa poprawie jesli kliknie sie na text
@@ -653,225 +630,81 @@
 //    dopóki nie kliknie sie znowu na resizera
 
 /*
-//shape config
-      const shapeConfig = document.querySelector(".object-menu");
-      let borderType;
+.create-new-text {
+  background: none;
+  width: 120px;
+  height: 40px;
+  border: 1px solid $secondary-color;
+  font-family: "Roboto";
+  font-size: 18px;
+  transition: background-color 0.5s ease-in-out;
+  margin: 20px auto 20px auto;
+  text-align: center;
+  display: block;
 
-      config.addEventListener("click", updateShape);
+  &:hover {
+    background: #f2f2f2;
+  }
+}
 
-      function updateShape(e) {
-        let currentlyStyling;
-        currentlyStylingObject = false;
+.text-input-wrapper {
+  color: $secondary-color;
+  margin: 10px auto 20px auto;
+  width: 500px;
+  height: 130px;
 
-        currentlyStylingObject = e.target.parentNode.className;
-        currentlyStyling = document.querySelector("." + currentlyStylingObject);
+  .text-input {
+    resize: none;
+    width: 500px;
+    height: 130px;
+  }
+}
 
-        console.log(currentlyStyling);
-        //zobaczyc czy to array, wybrac 1szy el
+.wrap-options,
+.color-options-wrapper {
+  margin: 0 auto;
+  width: 380px;
+  height: 100px;
+  display: flex;
+  justify-content: space-evenly;
+}
 
-        shapeConfig.classList.remove("invisible");
-        let styleApply = document.querySelectorAll(".style-apply");
+.font-size-wrapper,
+.font-family-wrapper,
+.font-color-wrapper,
+.background-color-wrapper {
+  width: 150px;
+  height: 100px;
+  margin-top: 10px;
+  margin-left: 10px;
+}
 
-        let rand = Math.floor(Math.random() * 100000) + 200000;
-        console.log(currentlyStyling.style.backgroundColor);
+.color-options-wrapper {
+  display: flex;
+}
 
-        currentlyStyling.style.backgroundColor = "#" + rand;
+.background-color-wrapper {
+  margin-top: -11px;
+}
 
-        for (let i = 0; i < styleApply.length; i++) {
-          styleApply[i].addEventListener("change", function() {
-            const fillCheckbox = document.querySelector(".fill-chbox");
-            const borderCheckbox = document.querySelector(".border-chbox");
-            const fillColorInput = document.querySelector(".shape-color-input")
-              .value;
-            const borderColorInput = document.querySelector(
-              ".border-color-input"
-            ).value;
-            const shapeBorderWidth = document.querySelector(
-              ".shape-border-width-input"
-            ).value;
-            const borderRadius1 = document.querySelector(".border-radius1")
-              .value;
-            const borderRadius2 = document.querySelector(".border-radius2")
-              .value;
-            const borderRadius3 = document.querySelector(".border-radius3")
-              .value;
-            const borderRadius4 = document.querySelector(".border-radius4")
-              .value;
+.color-preview,
+.background-preview {
+  width: 25px;
+  height: 25px;
+  margin: 8px auto 0 auto;
+  border: 1px solid #e2e2e2;
+}
 
-            const shapeShadowX = document.querySelector(".shape-shadow-x")
-              .value;
-            const shapeShadowY = document.querySelector(".shape-shadow-y")
-              .value;
-            const shapeShadowB = document.querySelector(".shape-shadow-b")
-              .value;
+.font-size-input,
+.select-font {
+  margin-top: 5px;
+}
 
-            ////////
-            // Pododawać img krawędzi na border radiusie
+.select-font {
+  width: 140px;
+}
 
-            //console.log(fillCheckbox.checked);
-
-            //bg, border
-
-            //console.log(borderType);
-
-            /*
-            if (fillCheckbox.checked) {
-              currentlyStyling.style.backgroundColor = fillColorInput;
-            } else {
-              currentlyStyling.style.backgroundColor = "transparent";
-            }
+.wrap-options {
+  display: flex;
+}
 */
-/*
-if (borderCheckbox.checked) {
-  currentlyStyling.style.border =
-    borderType +
-    " " +
-    borderColorInput +
-    " " +
-    shapeBorderWidth +
-    "px";
-} else {
-  currentlyStyling.style.border = "none";
-}
-
-currentlyStyling.style.borderRadius = `${borderRadius1}px ${borderRadius2}px ${borderRadius3}px ${borderRadius4}px`;
-
-setStyle(fillColorInput);
-});
-}
-
-function setStyle(fillColorInput) {
-currentlyStyling.style.backgroundColor = fillColorInput;
-}
-}
-const borderStyleWrapper = document.querySelectorAll(".brdr");
-
-borderStyleWrapper.forEach(function(border) {
-//console.log(border);
-border.addEventListener("click", function() {
-borderType = this.classList[0];
-updateShape();
-});
-});
-*/
-
-/*
-let isResizing = false;
-      //dac foreach na diva i parent diva
-
-      let divChild = divParent.children[0];
-
-      objArr = [divChild, divParent];
-      objArr.forEach(function(obj) {
-        
-     function resizableDiv() {
-          resizerNE.addEventListener("mousedown", initDrag, false);
-          resizerSE.addEventListener("mousedown", initDrag, false);
-          resizerN.addEventListener("mousedown", initDrag, false);
-          resizerS.addEventListener("mousedown", initDrag, false);
-          resizerE.addEventListener("mousedown", initDrag, false);
-
-          let className;
-          resizerNE.addEventListener("mousedown", function() {
-            className = "resizerNE";
-          });
-
-          resizerSE.addEventListener("mousedown", function() {
-            className = "resizerSE";
-          });
-
-          resizerN.addEventListener("mousedown", function() {
-            className = "resizerN";
-          });
-
-          resizerS.addEventListener("mousedown", function() {
-            className = "resizerS";
-          });
-
-          resizerE.addEventListener("mousedown", function() {
-            className = "resizerE";
-          });
-
-          function initDrag(e) {
-            startX = e.clientX;
-            startY = e.clientY;
-
-            startWidth = parseInt(
-              document.defaultView.getComputedStyle(obj).width,
-              10
-            );
-
-            startHeight = parseInt(
-              document.defaultView.getComputedStyle(obj).height,
-              10
-            );
-            document.documentElement.addEventListener(
-              "mousemove",
-              doDrag,
-              false
-            );
-            document.documentElement.addEventListener(
-              "mouseup",
-              stopDrag,
-              false
-            );
-          }
-
-          //console.log("x " + startX); // odległość od lewej strony
-          //console.log("y " + startY); // odległość od góry
-          //console.log("w " + startWidth); // szerokość elementu, update po podnesieniu myszki
-          //console.log("h " + startHeight); // wysokość elementu, update po podnesieniu myszki
-          //console.log(e.clientX); // pozycja myszki oś X
-          //console.log(e.clientY); // pozycja myszki oś Y
-
-          function doDrag(e) {
-            rect = divParent.getBoundingClientRect();
-            isResizing = true;
-            if (className == "resizerSE") {
-              obj.style.width = startWidth + e.clientX - startX + "px";
-              obj.style.height = startHeight + e.clientY - startY + "px";
-            } else if (className == "resizerE") {
-              obj.style.width = startWidth + e.clientX - startX + "px";
-            } else if (className == "resizerS") {
-              obj.style.height = startHeight + e.clientY - startY + "px";
-            } else if (className == "resizerNE") {
-              obj.style.height = startHeight + (e.clientY - startY) + "px";
-            } else if (className == "resizerN") {
-              divParent.style.height = startHeight + e.clientY;
-              divParent.style.top = e.clientY;
-
-              console.log("ec: " + e.clientY);
-              console.log("starty: " + startY);
-
-              //obj.style.height = startHeight + e.clientY + "px";
-              //divParent.style.top = e.clientY + "px";
-              //console.log(e.clientY);
-              //console.log(divParent.style.top);
-            }
-          }
-
-          function stopDrag() {
-            document.documentElement.removeEventListener(
-              "mousemove",
-              doDrag,
-              false
-            );
-            document.documentElement.removeEventListener(
-              "mouseup",
-              stopDrag,
-              false
-            );
-          }
-
-          resizerSE.addEventListener("mouseup", stopResizing);
-          resizerNE.addEventListener("mouseup", stopResizing);
-          resizerS.addEventListener("mouseup", stopResizing);
-
-          function stopResizing() {
-            isResizing = false;
-          }
-        }
-
-        resizableDiv();
-      });
-      */
